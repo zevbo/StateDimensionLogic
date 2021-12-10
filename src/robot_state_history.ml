cf6a2d096b17b0e139b11eaad5286f7f25d4e1fc
zevbo <zevminskyprimus@gmail.com>
2021-12-10 00:23:15 -0500

    too schmexy

--- src/robot_state_history.ml
+++ src/robot_state_history.ml
@@ -6,21 +6,54 @@ type t =
   { past_states : (tick, Robot_state.t, Int.comparator_witness) Map.t
   ; curr_state : Robot_state.t
   ; tick : tick
+  ; sd_categories : Sd.Packed.t list array
   ; max_length : int
   }
 
-let create ~max_length =
-  if max_length <= 0
+(* 1 -> 1, 2 -> 2, 3 -> 2, 7 -> 3 *)
+let num_categories len = Int.ceil_log2 (len + 1)
+
+(* cateogry 0 -> 0, category 1 -> 2, category 2 -> 6, category 3 -> 14  *)
+let max_category_i category = Int.pow 2 (category + 1) - 2
+
+(* 1 -> category 0, 2 -> category 1, 3 -> category 1, 4 -> category 2, 7 -> category 2, 8 -> category 3 *)
+let len_to_category i = Int.floor_log2 i
+let real_store_len len = max_category_i (len_to_category len) + 1
+
+(* note: for the moment, default_length only matters if it's the max_length *)
+let create ?(default_length = 1) ?(sd_lengths = Map.empty (module Sd.Packed)) () =
+  if default_length <= 0
   then
     raise
       (Invalid_argument
          (Printf.sprintf
-            "~max_length in Robot_state_history.create must be positive. Given %i"
-            max_length));
+            "~default_length in Robot_state_history.create must be positive. Given %i"
+            default_length));
+  Map.iteri sd_lengths ~f:(fun ~key ~data ->
+      if data <= 0
+      then
+        raise
+          (Invalid_argument
+             (Printf.sprintf
+                "entries in ~sd_lengths in Robot_state_history.create must be positive. \
+                 Given %i for key %s"
+                data
+                (Sd.Packed.to_string key))));
+  let max_length =
+    Option.value_exn
+      (List.max_elt
+         ~compare:Int.compare
+         (default_length :: List.map ~f:snd (Map.to_alist sd_lengths)))
+  in
+  let sd_categories = Array.create ~len:(num_categories max_length) [] in
+  Map.iteri sd_lengths ~f:(fun ~key ~data ->
+      let category = len_to_category data in
+      Array.set sd_categories category (key :: Array.get sd_categories category));
   { past_states = Map.empty (module Int)
-  ; max_length
   ; tick = 0
   ; curr_state = Robot_state.empty
+  ; sd_categories
+  ; max_length
   }
 ;;
 
@@ -29,8 +62,8 @@ let next_tick t n =
   (n + 1) % (t.max_length - 1)
 ;;
 
-let nth_to_tick t n =
-  let t = (t.tick - n) % (t.max_length - 1) in
+let nth_to_tick t ?(tick = t.tick) n =
+  let t = (tick - n) % (t.max_length - 1) in
   t
 ;;
 
@@ -70,19 +103,41 @@ let memp_past t n sd =
   | Some state -> Some (Robot_state.memp state sd)
 ;;
 
-let add_state t =
+let add_empty_state t =
   if t.max_length = 1
   then { t with curr_state = Rs.empty }
   else (
     let tick = next_tick t t.tick in
     let curr_state = Robot_state.empty in
     let past_states = Map.set t.past_states ~key:t.tick ~data:t.curr_state in
-    { t with tick; curr_state; past_states })
+    (* length is 1 we do 0, length is 2 we do 1, length is 3 we do 1, length is 4 we do 2 *)
+    let l = length t in
+    let categories = List.range 0 (num_categories l) in
+    let trimmed =
+      List.fold_left
+        categories
+        ~f:(fun map category ->
+          let i = max_category_i category + 1 in
+          if i >= l
+          then map
+          else (
+            let tick = nth_to_tick t ~tick i in
+            let state = Map.find_exn map tick in
+            let sds_to_delete = Array.get t.sd_categories category in
+            let state =
+              List.fold sds_to_delete ~f:(fun state sd -> Rs.removep state sd) ~init:state
+            in
+            Map.set map ~key:tick ~data:state))
+        ~init:past_states
+    in
+    { t with tick; curr_state; past_states = trimmed })
 ;;
 
 let use t ?(to_use = None) (state : Robot_state.t) =
   { t with curr_state = Robot_state.use ~to_use t.curr_state state }
 ;;
+
+let add_state t rs = use (add_empty_state t) rs
 
 type sexpable_rsh =
   { states : Robot_state.t list
