d19a57011ccf10eadb19aadd3471f304f23e2d5a
zevbo <zevminskyprimus@gmail.com>
2021-12-12 22:52:40 -0500

    reogranization

---
+++ run_simple_example/run_simple_example.ml
@@ -0,0 +1,1 @@
+let () = Simple_example.Main.run ()
---
+++ simple_example/main.ml
@@ -0,0 +1,16 @@
+open! Core
+open! Src
+open! State_estimators
+
+let print =
+  [%map_open.Sd_lang
+    let v = sd Sds.v
+    and x = sd Sds.x
+    and light_on = sd Sds.light_on in
+    printf "v: %f, x: %f, light on?: %b\n" v x light_on;
+    Rs.empty]
+;;
+
+let print_est = Est.create print (Set.empty (module Sd.Packed))
+let model = Seq_model.create [ Update_v.est; Update_x.est; Light_on.est; print_est ]
+let run () = Seq_model.run model ~ticks:(Some 100)
