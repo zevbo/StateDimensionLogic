f5fb5a017070456c1c5f4c843b0bff1aadb663c4
zevbo <zevminskyprimus@gmail.com>
2022-01-21 17:45:23 -0500

    adding stuff to sd_node

--- sd_logic/sd_node.ml
+++ sd_logic/sd_node.ml
@@ -7,10 +7,26 @@ type t =
 
 let create logic sds_estimating = { logic; sds_estimating }
 
-type safety =
-  | Safe
-  | Warnings
+type one_safety =
   | Unsafe
+  | Warnings
+  | Safe
+[@@deriving compare]
+
+let max_safety s1 s2 = if compare_one_safety s1 s2 > 0 then s1 else s2
+
+type safety_info = { max_safety : one_safety }
+
+type safety =
+  { missing_sd : one_safety
+  ; extra_sd : one_safety
+  ; info : safety_info
+  }
+
+let create_safety ?(default = Safe) ?(missing_sd = default) ?(extra_sd = default) =
+  let info = { max_safety = max_safety missing_sd extra_sd } in
+  { missing_sd; extra_sd; info }
+;;
 
 exception Missing_sd of string
 exception Extra_sd of string
@@ -18,7 +34,7 @@ exception Extra_sd of string
 let execute ~safety t rsh =
   let estimated_state = Sd_lang.execute t.logic rsh in
   let expected_keys = t.sds_estimating in
-  (match safety with
+  (match safety.info.max_safety with
   | Unsafe -> ()
   | Safe | Warnings ->
     let missing =
@@ -28,6 +44,12 @@ let execute ~safety t rsh =
       Set.find
         ~f:(fun key -> not (Set.mem expected_keys key))
         (Robot_state.keys estimated_state)
+    in
+    let run_check one_safety result exc msg = 
+      match one_safety, result with 
+      | Unsafe, _ | _, None -> ()
+      | Warnings, Some sd -> printf msg (Sd.Packed.to_string sd)
+      | Safe, Some sd -> raise (exc (Sd.))
     in
     (match safety, missing, extra with
     | Unsafe, _, _ -> (* should never reach here *) ()
